1f2b2e47df3f8d6c6d3ffb97b8d1934e928a4993
Utku Koprubasi <azizutku98@hotmail.com>
2024-08-11 17:44:33 +0300

    veri çekme tamamlandı

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,56 @@
-import './App.css';
+import { Badge, ListGroup } from "react-bootstrap";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { BoxArrowUpRight } from "react-bootstrap-icons";
 
 function App() {
-  return (
-  <section className='container mt-5' >
-  <div className='row'>
-    <div className='col'>
-    <ol className="list-group list-group-numbered">
-  <li className="list-group-item d-flex justify-content-between align-items-start">
-    <div className="ms-2 me-auto">
-      <div className="fw-bold">Subheading</div>
-      Content for list item
-    </div>
-    <span className="badge text-bg-primary rounded-pill">14</span>
-  </li>
-  <li className="list-group-item d-flex justify-content-between align-items-start">
-    <div className="ms-2 me-auto">
-      <div className="fw-bold">Subheading</div>
-      Content for list item
-    </div>
-    <span className="badge text-bg-primary rounded-pill">14</span>
-  </li>
-  <li className="list-group-item d-flex justify-content-between align-items-start">
-    <div className="ms-2 me-auto">
-      <div className="fw-bold">Subheading</div>
-      Content for list item
-    </div>
-    <span className="badge text-bg-primary rounded-pill">14</span>
-  </li>
-</ol>
-    </div>
-   </div>
-   </section>
+  const [veri, veriGuncelle] = useState({ hits: [] });
 
+  useEffect( ()=> {
+
+    const veriCek = async ()=> {
+      const sonuc = await fetch("https://hn.algolia.com/api/v1/search?query=javascript")
+      const jsSonuc = await sonuc.json()
+
+      veriGuncelle( jsSonuc )
+    }
+
+    veriCek()
+
+  }, [] )
+
+
+
+  return (
+    <section className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <h1>Javascript Haberler</h1>
+
+          <ListGroup as="ol" numbered>
+            {veri.hits.map( (haber) => {
+              return (
+                <ListGroup.Item
+                key={haber.story_id}
+                  as="li"
+                  className="d-flex justify-content-between align-items-start"
+                >
+                  <div className="ms-2 me-auto">
+                    <div className="fw-bold">{haber.title}</div>
+                    <strong>Yazar:</strong> {haber.author}  <a target="_blank" href={haber.url}> <BoxArrowUpRight /> </a>
+                  </div>
+                  <Badge bg="primary" pill>
+                    {haber.points}
+                  </Badge>
+                </ListGroup.Item>
+              )
+            }
+            )}
+          </ListGroup>
+        </div>
+      </div>
+    </section>
   );
 }
-
 
 export default App;
