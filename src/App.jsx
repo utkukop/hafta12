995a594d6dc191d255930cd8d5e44e409d130f7d
Utku Koprubasi <azizutku98@hotmail.com>
2024-08-11 16:19:46 +0300

    Bootstrap paketi projeye eklendi.

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+  <section className='container mt-5' >
+  <div className='row'>
+    <div className='col'>
+    <ol className="list-group list-group-numbered">
+  <li className="list-group-item d-flex justify-content-between align-items-start">
+    <div className="ms-2 me-auto">
+      <div className="fw-bold">Subheading</div>
+      Content for list item
+    </div>
+    <span className="badge text-bg-primary rounded-pill">14</span>
+  </li>
+  <li className="list-group-item d-flex justify-content-between align-items-start">
+    <div className="ms-2 me-auto">
+      <div className="fw-bold">Subheading</div>
+      Content for list item
+    </div>
+    <span className="badge text-bg-primary rounded-pill">14</span>
+  </li>
+  <li className="list-group-item d-flex justify-content-between align-items-start">
+    <div className="ms-2 me-auto">
+      <div className="fw-bold">Subheading</div>
+      Content for list item
+    </div>
+    <span className="badge text-bg-primary rounded-pill">14</span>
+  </li>
+</ol>
+    </div>
+   </div>
+   </section>
+
+  );
+}
+
+
+export default App;
